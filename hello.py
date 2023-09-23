914eb3f21960a623a59b872c7387f7ae7679a194
niishaaa <niishaa.malik@mail.utoronto.ca>
2023-09-23 16:57:54 -0400

    Activity 3 all files to make webpage

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', 
+                           current_time=datetime.utcnow())
 
-#Example 2-2
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
