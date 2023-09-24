109552d3543f82e72b4905bab39b13e48ec0a4e5
niishaaa <niishaa.malik@mail.utoronto.ca>
2023-09-24 14:06:08 -0400

    Activity 4: Adding UofT email field and validation

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError
 
 
 app = Flask(__name__)
@@ -12,17 +12,28 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(FlaskForm):
+def customEmailValidation(FlaskForm, field):
+    if '@' not in field.data:
+        raise ValidationError('Please include an \'@\' in the email address. \'{}\' is missing an \'@\'.'.format(field.data))
+
+class NameEmailForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), customEmailValidation])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = NameEmailForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
-            flash('Looks like you changed your name')
+            flash('Looks like you changed your name!')
         session['name'] = form.name.data
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
